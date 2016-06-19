b75f2d45366e39dc4d50c21c43585121202b943c
andro999b <andro999b@gmail.com>
2016-06-19 11:58:17 +0300

    refactoring

--- worker.js
+++ worker.js
@@ -8,11 +8,30 @@ const imageminMozjpeg = require('imagemin-mozjpeg');
 const share = require("./share");
 const constants = require("./consts");
 
-const pfs = pify(fs);
-const pmongodb = pify(mongodb.MongoClient);
-
 //conver image
 const conver = (width, height, operation) => {
+    //remove aplha channel
+    const removeAplha = image => image.background("white").flatten();
+
+    //nice crop
+    const crop = image => new Promise((resolve) => {
+        image.size((err, size) => {
+            if(err) throw err;
+
+            let iw = size.width, ih = size.height;
+            let scale = Math.max(width / iw, height / ih)
+            
+            image.scale(iw * scale, ih * scale)
+            if(iw > ih) image.gravity("Center");
+            image.crop(width, height);
+
+            resolve(image)
+        })
+    })
+
+    //recate thumbnail(fast)
+    const thumb = (image) => image.thumbnail(width, height).quality(100);
+
     return pify((data, callback) => {
         let type = null;
         let {buffer, contentType} = data;
@@ -23,34 +42,19 @@ const conver = (width, height, operation) => {
             case "image/gif": type = "gif"; break;
         }
 
-
-        let image = gm(buffer, `image.${type}`);
-        image.size((err, size) => {
-            if (err) { callback(err); return; }
-
-            image.background("white").flatten()
-
-            let iw = size.width, ih = size.height;
-            let vertical = iw < ih;
-            switch (operation) {
-                case "c": {//crop
-                    if(vertical) {
-                        let scale = iw > width ? height / ih : width / iw;
-                        image.scale(iw * scale, ih * scale).crop(width, height);
-                    } else {
-                        let scale = ih > height ? width / iw :  height / ih; 
-                        image.scale(iw * scale, ih * scale).gravity("Center").crop(width, height);
-                    }
-                    break;
+        Promise.resolve(gm(buffer, `image.${type}`))
+            .then(removeAplha)
+            .then(image => {
+                switch (operation) {
+                    case "c": return crop(image);
+                    case "s"://scale
+                    default: return thumb(iamge);
                 }
-                case "s"://scale
-                default: {
-                    image.resize(width, height);
-                }
-            }
-
-            image.noProfile().toBuffer("JPEG", callback);
-        })
+            })
+            .then(image => 
+                image.noProfile().toBuffer("JPEG", callback)
+            )
+            .catch(callback);
     })
 }
 
@@ -66,17 +70,31 @@ const download = pify(function (bucket, id, callback) {
 
 const minify = imageminMozjpeg({ quality: constants.JPEG_QUALITY });
 
-module.exports = () => {
-    pmongodb.connect(constants.MONGO_URI)
-        .then(db => new mongodb.GridFSBucket(db, { bucketName: constants.BACKET_NAME }))
-        .then(bucket => {
+const startGenerationQueue = bucket => {
+            //generation task
             const generate = (params, filePath) => {
                 let {id, width, height, operation} = params;
 
                 return download(bucket, id)
                     .then(conver(width, height, operation))
                     .then(minify)
-                    .then(buf => pfs.writeFile(filePath, buf))
+                    .then(buf => pify(fs.writeFile)(filePath, buf))
+            }
+
+            //generation finish
+            const generationFinish = (params, fileName, success) => {
+                let msg =  success ? 
+                    `[Generator Worker] Image ${fileName} generated` :
+                    `[Generator Worker] Fail to generate image ${fileName}`;
+
+                return  () => {
+                    console.log(msg)
+                    inprogress.delete(fileName);
+                    params.success = success;
+                    process.send(params);
+                    activeCount--;
+                    next();
+                }
             }
 
             let inprogress = new Set();//current in progress tasks
@@ -84,7 +102,8 @@ module.exports = () => {
             let queue = [];//queue of generation tasks
 
             const next = () => {
-                if (activeCount >= constants.PARALLEL_GENERATION_COUNT || !queue.length)
+                if (activeCount >= constants.PARALLEL_GENERATION_COUNT ||
+                    !queue.length)
                     return;
 
                 let params = queue.pop();
@@ -93,39 +112,29 @@ module.exports = () => {
 
                 activeCount++;
                 generate(params, filePath)
-                    .then(() => {
-                        inprogress.delete(fileName);
-                        console.log(`[Generator Worker] Image ${fileName} generated`);
-
-                        params.success = true;
-                        process.send(params);
-
-                        activeCount--;
-                        next();
-                    })
+                    .then(generationFinish(params, fileName, true))
                     .catch(err => {
-                        inprogress.delete(fileName)
-                        console.log(`[Generator Worker] Fail to generate image ${fileName}`, err)
-
-                        params.success = false;
-                        process.send(params);
-
-                        activeCount--;
-                        next();
+                        generationFinish(params, fileName, false)();
+                        console.log(err);
                     })
-
             }
 
             process.on("message", params => {
                 if (inprogress.has(params.fileName))
                     return;//do not add unqueued file
-                
+
                 inprogress.add(params.fileName);
 
                 queue.push(params);
                 next();
             });
-        })
+        }
+
+module.exports = () => {
+    pify(mongodb.MongoClient)
+        .connect(constants.MONGO_URI)
+        .then(db => new mongodb.GridFSBucket(db, { bucketName: constants.BACKET_NAME }))
+        .then(startGenerationQueue)
         .then(() => console.log('Generator Worker started'))
         .catch(err => console.log('Fail to start worker', err))
 }

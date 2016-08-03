52748b159e750db3248976c239fd1602ffbf1cb3
andro <andro999b@gmail.com>
2016-08-03 11:48:18 +0300

    fix mongodb address

--- consts.js
+++ consts.js
@@ -16,7 +16,7 @@ module.exports = {
     SERVER_PORT: parseInt(process.env.SERVER_PORT) || 8080,
     TEMP_DIR: './temp', 
     FILE_DIR: './files',
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://192.168.4.218/lardi_files?', //'mongodb://172.16.2.2:27017/lardi_files'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://192.168.4.217/lardi_files?replicaSet=lardi_files', //'mongodb://172.16.2.2:27017/lardi_files'
     BACKET_NAME: process.env.GRIDFS_BACKET || "marketgoods",
     CACHE_CONTROL: `max-age=${7 * 24 * 3600}, public`,
     JPEG_QUALITY:  process.env.JPEG_QUALITY || 90,
